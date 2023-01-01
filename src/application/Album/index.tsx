0219c27b0e92124fc5f97e27f4d0d2bf804594a0
tangyijun <2537196761@qq.com>
2023-01-01 11:16:00 +0800

    feat:Album组件代码的封装优化

--- src/application/Album/index.tsx
+++ src/application/Album/index.tsx
@@ -1,4 +1,4 @@
-import React, { memo, useState, useRef, useEffect } from "react";
+import React, { memo, useState, useRef, useEffect, useCallback } from "react";
 import { Container, Menu, TopDesc, SongList, SongItem } from './style';
 import { CSSTransition } from 'react-transition-group';
 import { useNavigate, useParams } from "react-router-dom";
@@ -34,7 +34,7 @@ const Album: React.FC = () => {
   }, [id]);
 
   //滚动逻辑
-  const handleScroll = (pos: { x: number, y: number }) => {
+  const handleScroll = useCallback((pos: { x: number, y: number }) => {
     let minScrollY = -HEADER_HEIGHT;
     let percent = Math.abs(pos.y / minScrollY);
     let headerDom = headerEl.current;
@@ -50,6 +50,100 @@ const Album: React.FC = () => {
       setTitle('歌单');
       setIsMarquee(false);
     }
+  }, [currentAlbum])
+
+  const renderTopDesc: React.FC = ({ }) => {
+    return (
+      <TopDesc background={currentAlbum.coverImgUrl}>
+        <div className="background">
+          <div className="filter"></div>
+        </div>
+        <div className="img_wrapper">
+          <div className="decorate"></div>
+          <img src={currentAlbum.coverImgUrl} alt="" />
+          <div className="play_count">
+            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+              <StepForwardOutlined />
+              <span className="count">{Math.floor(currentAlbum.subscribedCount / 1000) / 10} 万 </span>
+            </div>
+          </div>
+        </div>
+        <div className="desc_wrapper">
+          <div className="title">{currentAlbum.name}</div>
+          <div className="person">
+            <div className="avatar">
+              <img src={currentAlbum.creator.avatarUrl} alt="" />
+            </div>
+            <div className="name">{currentAlbum.creator.nickname}</div>
+          </div>
+        </div>
+      </TopDesc>
+    )
+  }
+
+  const renderMenu: React.FC = ({ }) => {
+    return (
+      <Menu>
+        <div>
+          <div className="icon">
+            <CommentOutlined />
+          </div>
+          评论
+        </div>
+        <div>
+          <div className="icon">
+            <LikeOutlined />
+          </div>
+          点赞
+        </div>
+        <div>
+          <div className="icon">
+            <PlusOutlined />
+          </div>
+          收藏
+        </div>
+        <div>
+          <div className="icon">
+            <MoreOutlined />
+          </div>
+          更多
+        </div>
+      </Menu>
+    )
+  }
+
+  const renderSongList: React.FC = ({ }) => {
+    return (
+      <SongList showBackground={false}>
+        <div className="first_line">
+          <div className="play_all">
+            <PlayCircleOutlined className="iconfont" />
+            <span>播放全部<span className="sum">(共{currentAlbum.tracks.length}首)</span></span>
+          </div>
+          <div className="add_list">
+            <PlusCircleOutlined className="iconfont" />
+            <span>收藏({getCount(currentAlbum.subscribedCount)})</span>
+          </div>
+        </div>
+        <SongItem>
+          {
+            currentAlbum.tracks.map((item, index) => {
+              return (
+                <li key={index}>
+                  <span className="index">{index + 1}</span>
+                  <div className="info">
+                    <span>{item.name}</span>
+                    <span>
+                      {getName(item.ar)} - {item.al.name}
+                    </span>
+                  </div>
+                </li>
+              )
+            })
+          }
+        </SongItem>
+      </SongList>
+    )
   }
 
   return (
@@ -66,85 +160,9 @@ const Album: React.FC = () => {
         <Header ref={headerEl} title={title} isMarquee={isMarquee} handleClick={() => { setShowStatus(false) }}></Header>
         <Scroll bounceTop={false} onScroll={handleScroll}>
           <div>
-            <TopDesc background={currentAlbum.coverImgUrl}>
-              <div className="background">
-                <div className="filter"></div>
-              </div>
-              <div className="img_wrapper">
-                <div className="decorate"></div>
-                <img src={currentAlbum.coverImgUrl} alt="" />
-                <div className="play_count">
-                  <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                    <StepForwardOutlined />
-                    <span className="count">{Math.floor(currentAlbum.subscribedCount / 1000) / 10} 万 </span>
-                  </div>
-                </div>
-              </div>
-              <div className="desc_wrapper">
-                <div className="title">{currentAlbum.name}</div>
-                <div className="person">
-                  <div className="avatar">
-                    <img src={currentAlbum.creator.avatarUrl} alt="" />
-                  </div>
-                  <div className="name">{currentAlbum.creator.nickname}</div>
-                </div>
-              </div>
-            </TopDesc>
-            <Menu>
-              <div>
-                <div className="icon">
-                  <CommentOutlined />
-                </div>
-                评论
-              </div>
-              <div>
-                <div className="icon">
-                  <LikeOutlined />
-                </div>
-                点赞
-              </div>
-              <div>
-                <div className="icon">
-                  <PlusOutlined />
-                </div>
-                收藏
-              </div>
-              <div>
-                <div className="icon">
-                  <MoreOutlined />
-                </div>
-                更多
-              </div>
-            </Menu>
-            <SongList showBackground={false}>
-              <div className="first_line">
-                <div className="play_all">
-                  <PlayCircleOutlined className="iconfont" />
-                  <span>播放全部<span className="sum">(共{currentAlbum.tracks.length}首)</span></span>
-                </div>
-                <div className="add_list">
-                  <PlusCircleOutlined className="iconfont" />
-                  <span>收藏({getCount(currentAlbum.subscribedCount)})</span>
-                </div>
-              </div>
-              <SongItem>
-                {
-                  currentAlbum.tracks.map((item, index) => {
-                    return (
-                      <li key={index}>
-                        <span className="index">{index + 1}</span>
-                        <div className="info">
-                          <span>{item.name}</span>
-                          <span>
-                            {getName(item.ar)} - {item.al.name}
-                          </span>
-                        </div>
-                      </li>
-                    )
-                  })
-                }
-              </SongItem>
-            </SongList>
+            {renderTopDesc({})}
+            {renderMenu({})}
+            {renderSongList({})}
           </div>
         </Scroll>
       </Container>
